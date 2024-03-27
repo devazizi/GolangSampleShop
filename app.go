7e53c573411cbea4ce70fe8b58e04b73ce868fde
Azizi <dev.azizimail@gmail.com>
2024-03-27 15:47:44 -0400

    add mysql to game

--- app.go
+++ app.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"app/infrastructures"
 	"app/router"
+	"fmt"
 
 	// "reflect"
 	// "fmt"
@@ -10,6 +12,14 @@ import (
 )
 
 func main() {
+	db_dsn := "gitea:<gitea>@tcp(127.0.0.1:3306)/gitea"
+	db := infrastructures.NewMySqlDB(db_dsn)
+	query, err := db.Query("SHOW TABLES LIKE 'a%';")
+	if err != nil {
+		panic(err.Error())
+	}
+	fmt.Println(query)
+	fmt.Println(db)
 	e := echo.New()
 	router.Routes(e)
 	// fmt.Println("hello world this is a simple golang application")
--- infrastructures/mysql.go
+++ infrastructures/mysql.go
@@ -1,1 +1,15 @@
 package infrastructures
+
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func NewMySqlDB(dsn string) *sql.DB {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
